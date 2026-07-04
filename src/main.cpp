6691336c50223548a1317027125cc1b94e861fda
Jhon Ore <jhon.ore@hotmail.com>
2026-07-04 00:09:54 -0500

    version 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,30 @@
 #include <Arduino.h>
 
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
+// put function definitions here:
+int myFunction1(int x, int y) {
+  return x + y;
+}
+// put function definitions here:
+int myFunction2(int x, int y) {
+  return x + y;
+}
+// put function definitions here:
+int myFunction3(int x, int y) {
+  return x + y;
+}
